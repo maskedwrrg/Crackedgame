482669ecffe49c8c946faa73c44e6af7d8ddceb1
Haze <127317838+maskedwrrg@users.noreply.github.com>
2026-02-23 12:03:31 -0500

    Allow all hosts for fetching scripts

--- config.ts
+++ config.ts
@@ -6,14 +6,5 @@ export default {
     // This helps prevent your server from being "IP logged" with malicious hosts.
     // If your IP doesn't need to be hidden, add "*" to disable this functionality.
     // The default hosts entries are script CDNs & sources that are commonly used.
-    fetchable: [
-        'raw.githubusercontent.com',
-        'gist.githubusercontent.com',
-        'cdnjs.cloudflare.com',
-        'cdn.jsdelivr.net',
-        'raw.githack.com',
-        'unpkg.com',
-        'localhost',
-        'esm.sh'
-    ]
+    fetchable: [*]
 }
